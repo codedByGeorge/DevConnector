1bebad9cd45ea1e6027079c5b5cbb8404ddde70d
George Strippoli <codedbygeorge@gmail.com>
2020-10-02 22:41:07 -0500

    More profile work

--- config/db.js
+++ config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
-mongoose.set('useFindAndModify', false);
+
 const connectDB = async () => {
 	try {
 		await mongoose.connect(db, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true,
+			useFindAndModify: false,
 		});
 
 		console.log('MongoDB Connected...');
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,4 +95,120 @@ router.post(
 	}
 );
 
+// @route       GET api/profile
+// @description Get all profiles
+// access       Public
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+		res.json(profiles);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route       GET api/profile/user/:user_id
+// @description Get all profile ID
+// access       Public
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate('user', ['name', 'avatar']);
+		if (!profile)
+			return res.status(400).json({ msg: 'There is no profile for this user' });
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind == 'ObjectId') {
+			return res.status(400).json({ msg: 'Profile not found' });
+		}
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route       DELETE api/profile
+// @description Delete profile, user & posts
+// access       Private
+router.delete('/', auth, async (req, res) => {
+	try {
+		// @TODO -- REMOVE USER POSTS
+		// Remove Profile
+		await Profile.findOneAndRemove({ user: req.user.id });
+		// Remove User
+		await Profile.findOneAndRemove({ _id: req.user.id });
+		res.json({ msg: 'User deleted' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route       PUT api/profile/experience
+// @description Add profile experience
+// access       Private
+router.put(
+	'/experience',
+	[
+		auth,
+		[
+			check('title', 'Title is required').not().isEmpty(),
+			check('company', 'Company is required').not().isEmpty(),
+			check('from', 'From Date is required').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		} = req.body;
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+			profile.experience.unshift(newExp);
+			await profile.save();
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
+// @route       DELETE api/profile/experience/:exp_id
+// @description Delete experience from profile
+// access       Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+		// Get REMOVE index
+		const removeIndex = profile.experience
+			.map((item) => item.id)
+			.indexOf(req.params.exp_id);
+		profile.experience.splice(removeIndex, 1);
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
 module.exports = router;
