1e464d3dd013be95c8ec9ba8b6c2fcb8468e2c0c
codeByGeorge <codedbygeorge@gmail.com>
2020-10-18 21:42:49 -0500

    Added Education & Experience

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -38,6 +40,8 @@ const App = () => {
 						<PrivateRoute exact path="/dashboard" component={Dashboard}></PrivateRoute>
 						<PrivateRoute exact path="/create-profile" component={CreateProfile}></PrivateRoute>
 						<PrivateRoute exact path="/edit-profile" component={EditProfile}></PrivateRoute>
+						<PrivateRoute exact path="/add-experience" component={AddExperience}></PrivateRoute>
+						<PrivateRoute exact path="/add-education" component={AddEducation}></PrivateRoute>
 					</Switch>
 				</section>
 			</Fragment>
--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 
 export const DashboardActions = () => {
   return (
-<div class="dash-buttons">
-				<Link to="/edit-profile" class="btn"
-					><i class="fas fa-user-circle text-primary"></i> Edit Profile</Link>
-				<Link to="/dd-experience" class="btn"
-					><i class="fas fa-briefcase text-primary"></i> Add Experience</Link>
-				<Link to="/add-education" class="btn"
-					><i class="fas fa-graduation-cap text-primary"></i> Add Education</Link>
+<div className="dash-buttons">
+				<Link to="/edit-profile" className="btn"
+					><i className="fas fa-user-circle text-primary"></i> Edit Profile</Link>
+				<Link to="/add-experience" className="btn"
+					><i className="fas fa-briefcase text-primary"></i> Add Experience</Link>
+				<Link to="/add-education" className="btn"
+					><i className="fas fa-graduation-cap text-primary"></i> Add Education</Link>
 			</div>
   )
-}
+} 
 
 export default DashboardActions;
